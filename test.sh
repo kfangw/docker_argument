ec2c47e5167df36c56957bd48cc31268c2cb6afa
kfangw <kfangw@gmail.com>
2019-06-03 11:57:12 +0900

    mount volume readonly

--- test.sh
+++ test.sh
@@ -8,4 +8,6 @@ docker run --rm -it $(docker build -q .) 1 2 3 4 --sum
 
 docker run --rm -it $(docker build -q .) 1 2 3 4 5 --sum
 
-docker run --rm -it --entrypoint /bin/bash $(docker build -q .)
+docker run --rm -it --entrypoint /bin/bash $(docker build -q .)  #override entrypoint
+
+docker run --rm -it -v $(pwd):/argument/data:ro --entrypoint /bin/bash $(docker build -q .) #override entrypoint and mount the volume readonly
